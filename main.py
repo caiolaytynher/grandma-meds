f180b9e1cc84503e052041bbae99f7f827c7d651
caiolaytynher <caiolaytynherbr@gmail.com>
2022-05-12 17:53:42 -0300

    Added docstrings to the functions

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ DATA = "/home/caio/Documents/python/grandma-meds/data.json"
 
 
 def load_data(filepath: str) -> dict:
+    """
+    Returns the contents of a json file as a python dictionary.
+    """
     try:
         with open(filepath, "r") as f:
             return json.load(f)
@@ -16,11 +19,19 @@ def load_data(filepath: str) -> dict:
 
 
 def dump_data(filepath: str, data: dict) -> None:
+    """
+    Append the contents of a python dictionary to an existing
+    json file.
+    """
     with open(filepath, "w") as f:
         json.dump(data, f, indent=2)
 
 
-def validate_int(prompt: str) -> int:
+def get_valid_int(prompt: str) -> int:
+    """
+    Continuously asks the user for an integer until the user
+    provides a valid one.
+    """
     while True:
         string = input(prompt)
         if not string.isdigit():
@@ -32,13 +43,16 @@ def validate_int(prompt: str) -> int:
 
 
 def add_med(data: dict) -> None:
+    """
+    Add a medicine to the data dictionary.
+    """
     name = input("Name: ").lower()
     if name in data:
         print("Medicine already added, continuing will update the system data.")
 
-    doses = validate_int("Total amount of doses: ")
-    daily_intake = validate_int("Daily intake: ")
-    remaining_doses = validate_int("Remaining Doses: ")
+    doses = get_valid_int("Total amount of doses: ")
+    daily_intake = get_valid_int("Daily intake: ")
+    remaining_doses = get_valid_int("Remaining Doses: ")
     date = datetime.datetime.now()
 
     data[name] = {
@@ -50,6 +64,9 @@ def add_med(data: dict) -> None:
 
 
 def remove_med(data: dict) -> None:
+    """
+    Removes a medicine of the data dictionary.
+    """
     name = input("Name: ")
     if name in data:
         del data[name]
@@ -58,6 +75,10 @@ def remove_med(data: dict) -> None:
 
 
 def list_meds(data: dict) -> None:
+    """
+    Lists all the medicines that has been added to the system
+    as well as they info.
+    """
     for name in data:
         print(f"{name.capitalize()}:")
         for key, value in data[name].items():
@@ -65,6 +86,10 @@ def list_meds(data: dict) -> None:
 
 
 def show_med_status(name: str, data: dict) -> None:
+    """
+    Show the current status of a specific medicine. The status is
+    the amount of days/doses left and the run out data.
+    """
     medication = data[name]
     previous_date = datetime.datetime.strptime(
         medication["last modification"][:10], "%Y-%m-%d"
@@ -83,6 +108,9 @@ def show_med_status(name: str, data: dict) -> None:
 
 
 def get_status(data: dict):
+    """
+    Show the status of one or all medicines.
+    """
     name = input("Name: ")
     if name == "" or name.lower() == "all":
         for medicine in data:
@@ -114,6 +142,6 @@ if __name__ == "__main__":
     if len(args) == 2:
         main(args[1])
     elif len(args) == 1:
-        print('Please provide a command.')
+        print("Please provide a command.")
     else:
         print("Please provide only one command at a time.")
